faf971691fabfe75f3971c103f34903a40a2d306
Shadman Jamil <shadman.jamil@gmail.com>
2025-12-14 03:47:18 +0100

    removed logo from forget password page

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -32,7 +32,7 @@ export class ForgotPasswordComponent implements OnInit, OnDestroy {
         selfServiceUnlock: true,
         multiOptionalFactorEnroll: false
       },
-      logo: ' ',
+      //logo: ' ',
       i18n: {
         en: {
           'primaryauth.title': 'Reset Password',
